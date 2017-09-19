a8dfb2f2a19880bfc8b9e9e8c633bb0c747a27ab
Niraj Kr Chauhan <niraj.1991@gmail.com>
2017-09-19 13:58:13 +0530

    mongo connection updated

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     'port': process.env.PORT || 3000,
     'apiKey': '45959522',
     'apiSecret': 'd22e32a919ac11b5a410ed94c2147057f584e012',
-    'mongodb': 'mongodb://admin:admin@localhost:27017/stream'
+    'mongodb': 'mongodb://fec4d2b21114ff9f4a24d668197f93a4:admin@4a.mongo.evennode.com:27017/fec4d2b21114ff9f4a24d668197f93a4'
 };
